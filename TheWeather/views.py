d684a7ff680e3681619bdd0c90700cc3758df38c
Jakub Grzegorczyk <boginow@gmail.com>
2019-11-18 13:54:08 +0100

    Added error checking, about to add delete button

--- TheWeather/views.py
+++ TheWeather/views.py
@@ -1,12 +1,59 @@
 from django.shortcuts import render
 import requests
-# Create your views here.
+from .models import City
+from .forms import CityForm
 
 def home(request):
 
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=52f8ff4361387d223395313e3bbeac63'
-    city = 'Las Vegas'
 
-    r = requests.get(url.format(city))
-    print(r.text)
-    return render(request, 'TheWeather/weather.html')
+    err_msg = ''
+    message = ''
+    message_classs = ''
+
+    if request.method == "POST":
+        form = CityForm(request.POST)
+
+        if form.is_valid():
+            existing_city = form.cleaned_data['name']
+            number_of_cities =  City.objects.filter(name=existing_city).count()
+
+            if number_of_cities == 0:
+                r = requests.get(url.format(existing_city)).json()
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'The given city does not exist'
+            else:
+                err_msg = 'The city already exist'
+
+        if err_msg:
+            message = err_msg
+            message_class = 'is-danger'
+        else:
+            message = 'City added succesfully'
+            message_class = 'is-success'
+
+    form = CityForm()
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+        r = requests.get(url.format(city)).json()
+        city_weather = {
+            'city': city.name,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+        weather_data.append(city_weather)
+
+    context = {
+        'weather_data': weather_data,
+        'form' : form,
+        'message': message,
+        'message_class': message_class
+    }
+
+    return render(request, 'TheWeather/weather.html', context)
